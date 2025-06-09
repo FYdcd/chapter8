b2de8f050550adaa42c35cd35c0ea0017f49f8e1
241005 <241005@std.hi-joho.ac.jp>
2025-06-09 12:09:16 +0900

    p339 オーバーロードまで終わった。

--- src/Main.java
+++ src/Main.java
@@ -1,23 +1,20 @@
 public class Main {
     public static void main(String[] args) { //System.out.println();
-        //勇者の生成
-        Hero h = new Hero();
-        //Heroに初期値をセット
-        h.name = "ユウト";
-        h.hp = 100;
-
-        Matango m1 = new Matango();
-        m1.hp=50;
-        m1.suffix='A';
-
-        Matango m2 = new Matango();
-        m2.hp=48;
-        m2.suffix='B';
-
-        h.slip();
-        m1.run();
-        m2.run();
-        h.sit(10);
-        h.run();
+       Sword s = new Sword();
+       s.name="炎の剣";
+       s.damage=10;
+       Hero h1= new Hero("ユウ");
+       System.out.println(h1.hp);
+       System.out.println(h1.name);
+       h1.sword=s;
+       Hero h2=new Hero("アサカ");
+       System.out.println(h2.hp);
+       System.out.println(h2.name);
+       Wizard w=new Wizard("スガワラ");
+       System.out.println(w.hp);
+       System.out.println(w.name);
+       w.heal(h1);
+       w.heal(h2);
+       w.heal(h2);
     }
 }
